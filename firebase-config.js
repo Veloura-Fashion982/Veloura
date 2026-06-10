681db9140ad87a109a667a64919ce4aeafea1125
veloura <v3082602@gmail.com>
2026-06-10 12:56:22 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+
+// Firebase config yahan apni values daalni hongi
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
